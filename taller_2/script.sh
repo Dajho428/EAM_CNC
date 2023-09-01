7134e3b191fa9dbb4c421e796c1caff2b7af89da
dajho428 <hernandez.jhonatan.2602@eam.edu.co>
2023-09-01 16:59:54 -0500

    Subiendo parcial

---
+++ taller_2/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+variable=${1}
+
+mensajes(){
+echo  "Hackeando la nasa"
+echo "Accediendo al sistema"
+echo "El numero es: "$variable
+}
+echo "XD"
+fecha(){
+echo "obteniendo fecha"
+date
+}
+
+mensajes
+fecha
+echo "------------------------------------------------ "
+#Documentacion
+#leer el archivo de los usuarios
+#cat /etc/passwd
